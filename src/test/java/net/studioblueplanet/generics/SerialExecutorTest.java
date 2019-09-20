85e1cee3eaa6a9ca302f0dd837e9492df71da274
scubajorgen <scubajorgen@gmail.com>
2019-09-20 22:32:39 +0200

    Removed unused import

--- src/test/java/net/studioblueplanet/generics/SerialExecutorTest.java
+++ src/test/java/net/studioblueplanet/generics/SerialExecutorTest.java
@@ -11,7 +11,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
